44676c2eb9a2b84fecdc0f4a018196daf592da5c
Anthony Cappello <ac.anthony.cappello@gmail.com>
2018-10-25 22:38:21 -0400

    trying to login

--- app/src/main/java/com/example/anthony/myapplication/EndoLogin.java
+++ app/src/main/java/com/example/anthony/myapplication/EndoLogin.java
@@ -1,4 +1,25 @@
 package com.example.anthony.myapplication;
 
+import com.moomeen.endo2java.EndomondoSession;
+import com.moomeen.endo2java.error.InvocationException;
+import com.moomeen.endo2java.error.LoginException;
+import com.moomeen.endo2java.model.AccountInfo;
+import com.moomeen.endo2java.model.DetailedWorkout;
+import com.moomeen.endo2java.model.Workout;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 public class EndoLogin {
+
+    String userName=null;
+    String password=null;
+
+    EndomondoSession session = new EndomondoSession(userName, password);
+    try{
+        session.login();
+    }catch(LoginException e){
+        LOG.error("exception while trying to login user: {}", userName, e);
+    }
+
+
 }
