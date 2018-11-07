5dfdca42b9801e9c3fe197989f8c6c5d1e88d61d
Anna <atashbiz.anahita@gmail.com>
2018-11-07 11:06:51 -0500

    graph by manually entered data

--- app/src/main/java/com/example/anthony/myapplication/PredictActivity.java
+++ app/src/main/java/com/example/anthony/myapplication/PredictActivity.java
@@ -1,13 +1,39 @@
 package com.example.anthony.myapplication;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class PredictActivity extends AppCompatActivity {
-    //blank acttivity
+
+    private LineGraphSeries<DataPoint> series;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_predict);
+        setContentView(R.layout.activity_main);
+
+       // x:time , y=distance
+
+
+        GraphView graph=(GraphView) findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(1, 5),
+                new DataPoint(2, 10),
+                new DataPoint(3, 15),
+                new DataPoint(4, 12),
+                new DataPoint(5, 6)
+        });
+     
+        graph.addSeries(series);
+
+
     }
 }
