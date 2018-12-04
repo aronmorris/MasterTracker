ff02b64e271de5510cc5144de134a40f35e4cb3c
Anthony Cappello <ac.anthony.cappello@gmail.com>
2018-12-04 10:09:01 -0500

    Spinners

--- app/src/main/java/com/example/anthony/myapplication/PredictActivity.java
+++ app/src/main/java/com/example/anthony/myapplication/PredictActivity.java
@@ -4,6 +4,8 @@ package com.example.anthony.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -19,6 +21,34 @@ public class PredictActivity extends AppCompatActivity {
         setContentView(R.layout.activity_predict);
         Intent intent = getIntent();
         user = (User)intent.getSerializableExtra("User");
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner3_Month);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.month_name_choice, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        final Spinner spinner2 = (Spinner) findViewById(R.id.spinner4_TempStat);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_weather = ArrayAdapter.createFromResource(this,
+                R.array.graph_extra_choice, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner2.setAdapter(adapter_weather);
+
+        final Spinner spinner3 = (Spinner) findViewById(R.id.spinner5);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_endomondo = ArrayAdapter.createFromResource(this,
+                R.array.endomondo_selection, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner3.setAdapter(adapter_endomondo);
+
         /**
          * TODO display selected weather data and sport data on same graph
          * TODO make some kind of correlation between the data
